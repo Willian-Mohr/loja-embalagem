3a620126107672894f5fc7ab79caebcbea38b458
Willian Mohr <willianbmohr@gmail.com>
2025-05-28 17:14:08 -0300

    Criando beans para gerencias as injeções

---
+++ api/src/main/java/com/manoel/embalagem/api/config/ApplicationConfig.java
@@ -0,0 +1,21 @@
+package com.manoel.embalagem.api.config;
+
+import com.manoel.embalagem.application.usecase.EmpacotarPedidoUseCase;
+import com.manoel.embalagem.domain.port.CaixaSelecaoServicePort;
+import com.manoel.embalagem.infrastructure.service.DefaultCaixaSelecaoService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApplicationConfig {
+
+    @Bean
+    public CaixaSelecaoServicePort caixaSelecaoService() {
+        return new DefaultCaixaSelecaoService();
+    }
+
+    @Bean
+    public EmpacotarPedidoUseCase empacotarPedidoUseCase(CaixaSelecaoServicePort service) {
+        return new EmpacotarPedidoUseCase(service);
+    }
+}
