4d77c9a712d2e3b06b07cc35b6c3b57746015876
Willian Mohr <willianbmohr@gmail.com>
2025-05-28 17:14:32 -0300

    Migrando para Arrays

--- infrastructure/src/main/java/com/manoel/embalagem/infrastructure/service/DefaultCaixaSelecaoService.java
+++ infrastructure/src/main/java/com/manoel/embalagem/infrastructure/service/DefaultCaixaSelecaoService.java
@@ -6,10 +6,7 @@ import com.manoel.embalagem.domain.model.Pedido;
 import com.manoel.embalagem.domain.model.Produto;
 import com.manoel.embalagem.domain.port.CaixaSelecaoServicePort;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class DefaultCaixaSelecaoService implements CaixaSelecaoServicePort {
@@ -17,7 +14,7 @@ public class DefaultCaixaSelecaoService implements CaixaSelecaoServicePort {
     private final List<Caixa> caixasDisponiveis;
 
     public DefaultCaixaSelecaoService() {
-        caixasDisponiveis = List.of(
+        caixasDisponiveis = Arrays.asList(
                 new Caixa("Caixa 1", new Dimensao(30, 40, 80)),
                 new Caixa("Caixa 2", new Dimensao(80, 50, 40)),
                 new Caixa("Caixa 3", new Dimensao(50, 80, 60))
@@ -67,7 +64,7 @@ public class DefaultCaixaSelecaoService implements CaixaSelecaoServicePort {
                 // Nenhuma caixa pode conter os produtos restantes individualmente
                 Produto naoEmpacotavel = produtosRestantes.remove(0);
                 Map<String, List<String>> erroMap = new HashMap<>();
-                erroMap.put(null, List.of(naoEmpacotavel.getProdutoId()));
+                erroMap.put(null, Collections.singletonList(naoEmpacotavel.getProdutoId()));
                 caixasUsadas.add(erroMap);
             }
         }
