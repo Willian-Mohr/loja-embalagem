2560e1d23b4353617dc2a25fd9cf0dd0d88ea748
Willian Mohr <willianbmohr@gmail.com>
2025-05-28 13:20:54 -0300

    first commit

---
+++ src/main/java/com/manoel/loja_embalagem/LojaEmbalagemApplication.java
@@ -0,0 +1,13 @@
+package com.manoel.loja_embalagem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaEmbalagemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaEmbalagemApplication.class, args);
+	}
+
+}
